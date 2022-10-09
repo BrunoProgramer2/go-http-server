80a4621bd67db9d7b2361a2047c63aa8d26637a8
BrunoProgramer2 <98126497+BrunoProgramer2@users.noreply.github.com>
2022-10-08 22:12:44 -0300

    wazza

--- main.go
+++ main.go
@@ -1,20 +1,9 @@
 package main
 
 import (
-	"net/http"
-
-	"os"
-
 	"BrunoProgramer2.github.io/go-http-server/pkg"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-
-	if err != nil {
-		panic(err)
-	}
-	port := os.Getenv("PORT")
-	http.ListenAndServe(port, pkg.SetupRouter())
+	pkg.SetupRouter().Run()
 }
